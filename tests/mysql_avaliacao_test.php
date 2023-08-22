e43e76d1af085ddf936c118f42f221244aead89d
LeooDonaa <leonardo.dona08@mail.com>
2023-08-22 10:21:40 -0300

    corrigido

--- tests/mysql_avaliacao_test.php
+++ tests/mysql_avaliacao_test.php
@@ -4,9 +4,9 @@ require_once '../core/conexao_mysql.php';
 require_once '../core/sql.php';
 require_once '../core/mysql.php';
 
-insert_teste ('10', 'muito bom','1','3');
+insert_teste ('10', 'muito bom','1','2');
 buscar_teste();
-update_teste(1, '7', 'bom até','1','3');
+update_teste(1, '7', 'bom até','1','2');
 buscar_teste();
 
 //Teste inserção banco de dados

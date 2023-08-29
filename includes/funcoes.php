8256620268fa32a96e55718c287d0d057185320a
LeooDonaa <leonardo.dona08@mail.com>
2023-08-29 11:15:21 -0300

    aaaa

--- includes/funcoes.php
+++ includes/funcoes.php
@@ -1,7 +1,6 @@
 <?php
 function limparDados(string $dado) :string
 {
-    
     $tags = '<p><strong<i><ul><ol><li><h1><h2><h3>';
 
     $retorno = htmlentities(strip_tags($dado, $tags));
@@ -9,4 +8,5 @@ function limparDados(string $dado) :string
     return $retorno;
 
 }
+
 ?>

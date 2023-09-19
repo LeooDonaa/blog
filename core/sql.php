8256c4e34176955447998646d7107c43476663a8
LeooDonaa <leonardo.dona08@mail.com>
2023-09-19 10:39:17 -0300

    .

--- core/sql.php
+++ core/sql.php
@@ -3,13 +3,13 @@
 function insert(string $entidade, array $dados) : string
 {
     $instrucao = "INSERT INTO {$entidade}";
-    $campos = implode(', ', array_keys($dados));
-    $valores = implode(', ', array_values($dados));
+    $campos = implode(', ', array_keys($dados)); 
+    $valores = implode(', ', array_values($dados)); 
 
-    $instrucao .= " ({$campos})";
-    $instrucao .= " VALUES ({$valores})";
+    $instrucao .= " ({$campos})"; 
+    $instrucao .= " VALUES ({$valores})"; 
 
-    return $instrucao;
+    return $instrucao; 
 }
 
 // Função para criar uma instrução SQL de atualização de dados em uma entidade.
